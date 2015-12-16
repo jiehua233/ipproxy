afeb85dfa6c23ce882c7e57ae5109c94ae51fa77
jiehua <1753970025@qq.com>
2015-12-16 18:05:49 +0800

    add mulitprocessing and gevent

--- sniffer/__init__.py
+++ sniffer/__init__.py
@@ -3,12 +3,59 @@
 #
 # @author   jiehua233@gmail.com
 # @site     http://chenjiehua.me
-# @date     2015-12-15
+# @date     2015-12-16
 #
 
+import redis
+
+from sniffer.validate import Validator
+from etc.config import SNIFFER, KEY_PREFIX
 
 class Sniffer:
     """ Validate the proxy ip. """
 
-    def validate(self):
-        pass
+    def __init__(self):
+        self.proxy_type = sorted(SNIFFER['PROXY_TYPE'], reverse=True)
+        self.validate = Validate()
+
+    def run(self, proxyip):
+        proxy_set = self.classify(proxyip)
+        result = self.validate(proxy_set)
+        if SNIFFER['OUTPUT']:
+            self.save2file(result)
+
+        if SNIFFER['BACKEND'] != '':
+            self.save2redis(result)
+
+    def validate(self, proxy_set):
+        result = {}
+        for proxy_type in self.proxy_type:
+            proxy_list = list(proxy_set.get(proxy_type, set()))
+            result[proxy_type] = validator.run_in_multiprocess(proxy_list)
+
+        return result
+
+    def save2file(self, result):
+        """ 保存到文件 """
+        for proxy_type in result:
+            proxy_list = sorted(result[proxy_type].iteritems(), key=operator.itemgetter(1))
+            with open('./data/ipproxy-%s.txt' % proxy_type) as f:
+                for ip in proxy_list:
+                    f.write("%s\t%s\n" % ip[0], ip[1])
+
+    def save2redis(self, result):
+        """ 保存到redis """
+        self.redis = redis.StrictRedis(*SNIFFER['BACKEND'].split(':'))
+        # TODO
+
+    def classify(self, proxyip):
+        """ 根据匿名程度对ip进行去重分类 """
+        result = {}
+        for i in range(4):
+            result.setdefault(i, set())
+
+        for ip in proxyip:
+            ip_port = "%(ip)s:%(port)s" % ip
+            result[int(ip['type'])].add(ip_port)
+
+        return result
--- sniffer/validate.py
+++ sniffer/validate.py
@@ -1,41 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# @author   jiehua233@gmail.com
+# @site     http://chenjiehua.me
+# @date     2015-12-16
+#
 
 import time
+import logging
+import multiprocessing
 import requests
-from base import Base
+import gevent
+from gevent import monkey; monkey.patch_all()
+from etc.config import SNIFFER
 
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+class Validator:
 
-""" 定时刷新 """
+    def __init__(self):
+        self.target = SNIFFER['TARGET']
+        self.timeout = SNIFFER['TIMEOUT']
+        self.process_num = SNIFFER['process_num']
+        self.thread_num = SNIFFER['THREAD_NUM']
 
-class Validate(Base):
+    def run_in_multiprocess(self, proxy_list):
+        result_queue = multiprocessing.Queue()
+        proxy_partitions = self.partite_proxy(proxy_list)
+        process = []
+        for partition in proxy_partitions:
+            p = multiprocessing.Process(target=self.process_with_gevent, args=(result_queue, partition))
+            p.start()
+            process.append(p)
 
-    # r = [3, 2], 只处理高匿名,匿名代理ip
-    #r = range(4)[::-1]
-    r = [3]
+        for p in process:
+            p.join()
 
-    def reflesh(self):
-        # 优先处理高匿名代理
-        for i in self.r:
-            key = "proxy_ip_ping_%s" % i
-            ips = self.redis.zrange(key, 0, -1)
-            print key, len(ips)
-            for ip in ips:
-                try:
-                    start = time.time()
-                    if self._ping(ip):
-                        duration = time.time() - start
-                        self.redis.zadd(key, duration, ip)
-                        print "reflesh in sorted set"
-                    else:
-                        self.redis.zrem(key, ip)
-                        print "del from sorted set"
+        result = {}
+        for p in process:
+            result.update(result_queue.get())
 
-                except Exception, e:
-                    print e
+        return result
+
+    def partite_proxy(self, proxy_list):
+        result = []
+        step = len(proxy_list) / self.process_num
+        for i in range(0, len(proxy_list), step):
+            result.append(proxy_list[i:i+step])
+
+        return result
+
+    def process_with_gevent(self, result_queue, proxy_list):
+        jobs = [gevent.spawn(self.validate_job, proxy_list) for i in range(self.thread_num)]
+        gevent.joinall(jobs)
+        result = {}
+        for job in jobs:
+            result.update(job.value)
+
+        result_queue.put(result)
+
+    def validate_job(self, proxy_list):
+        result = {}
+        while len(proxy_list) > 0:
+            ip_port = proxy_list.pop()
+            is_valid, speed = self.validate(ip_port)
+            if is_valid:
+                result[ip_port] = speed
+
+        return result
+
+    def validate(self, ip_port):
+        logging.info("ping: %s" % ip_port)
+        proxies = {
+            "http": "http://%s" % ip_port,
+        }
+        try:
+            start = time.time()
+            r = requests.get(self.target, proxies=proxies, timeout=self.timeout)
+            if r.status_code == requests.codes.ok:
+                return True, time.time() - start
+
+        except Exception as e:
+            logging(e)
+
+        return False, 0
+
+
+
+
 
     def ping(self):
         # 优先处理高匿名代理
@@ -58,22 +108,3 @@ class Validate(Base):
                 except Exception, e:
                     print e
 
-    def _ping(self, ip_port):
-        print "ping: %s" % ip_port
-        #url = "http://cn.bing.com"
-        #url = "http://www.baidu.com"
-        url = "http://www.1yyg.com"
-        proxies = {
-            "http": "http://%s" % ip_port,
-        }
-        try:
-            r = requests.get(url, proxies=proxies, timeout=self.timeout)
-            if r.status_code == requests.codes.ok:
-                return True
-
-        except:
-            pass
-        #except Exception, e:
-        #    print e
-
-        return False
