ba41665b31f8bbdcd729f149031295788614d5f4
jiehua <1753970025@qq.com>
2016-09-05 18:10:49 +0800

    in case of fire

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# @author   jiehua233@gmail.com
-# @site     http://chenjiehua.me
-# @date     2015-12-15
+# @author:  jiehua233@gmail.com
+# @sitef:   http://chenjiehua.me
+# @date:    2016-09-05
 #
 
-from crawler import Crawler
-from sniffer import Sniffer
-from etc.logger import logger
-
+import argparse
+import multiprocessing
+from validate import Validator
 
 def main():
-    proxyips = Crawler.run()
-    logger.info('Crawler finish, total ip: %s', len(proxyips))
-    sniffer = Sniffer()
-    sniffer.run(proxyips)
+    args = parse_args()
+    validator = Validator(args.target, args.timeout, args.process_num, args.thread_num)
+    result = validator.run(args.input_csv)
+    list(result)
+
+
+def parse_args():
+    procs_num = multiprocessing.cpu_count()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_csv", help="the available proxy ip list, in csv format")
+    parser.add_argument("--target", default="http://www.baidu.com", help="target uri to validate proxy ip, default: http://www.baidu.com")
+    parser.add_argument("--timeout", type=int, default=30, help="timeout of validating each ip, default: 30s")
+    parser.add_argument("--process_num", type=int, default=procs_num, help="run in multi process, default: CPU cores")
+    parser.add_argument("--thread_num", type=int, default=100, help="run in multi thread of each process, default: 100")
+
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
     main()
